7552f80437a181b42d9a075dd66b7de2611e03f6
Janay Bailey <tjanay87@gmail.com>
2019-09-22 19:34:53 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,13 +1,24 @@
 # Write your code here.
-name =  ("Edsger","Ada","Charles","Alan","Grace","Linus","Matz")
-number = 0
-def batch_badger_creator()
-  name.each do |name|
-  puts "Hello, my name is #{name}."
+attendees =  ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+room_assignments = 1
+
+def badge_maker(name)
+   "Hello, my name is #{name}."
 end
 
-def assign_rooms()
-  while batch_badger_creator()
-  puts "Hello #{name}! You'll be assigned to room #{number}!"
-  number += 1 
+def batch_badge_creator(attendees)
+  attendees.map do |name|
+  "Hello, my name is #{name}."
+  end
+end
+
+def assign_rooms(attendees)
+  newArray = []
+  attendees.each do |name| 
+  "Hello #{attendees}! You'll be assigned to room #{room_assignments}!"
+  room_assignments += 1 
+  push.newArray
+  end
+    return newArray
+    end
 end
